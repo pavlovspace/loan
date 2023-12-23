4d5f3ff7389f195e988e631656f610c72705f856
Paul Pavlov <pavlov1space@gmail.com>
2023-12-23 16:12:50 +0100

    implemented slider

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import Slider from './module/slider'
+
+window.addEventListener('DOMContentLoaded', () => {
+    const slider = new Slider('.page', '.next')
+
+    slider.render()
+})
---
+++ src/js/module/slider.js
@@ -0,0 +1,37 @@
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page)
+        this.slides = Array.from(this.page.children)
+        this.btns = document.querySelectorAll(btns)
+        this.slideIndex = 1
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1
+        } 
+        if (n < 1) {
+            this.slideIndex = this.slides.length
+        }
+
+        this.slides.forEach((element) => {
+            element.style.display = 'none'
+        })
+
+        this.slides[this.slideIndex - 1].style.display = 'block'
+    }
+
+    plusSlides(n) {
+        this.showSlides((this.slideIndex += n))
+    }  
+
+    render() {
+        this.btns.forEach((element) => {
+            element.addEventListener('click', () => {
+                this.plusSlides(1)
+            })
+        })
+
+        this.showSlides(this.slideIndex)
+    }
+}
