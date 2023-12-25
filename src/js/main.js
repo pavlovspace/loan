b0f33eafa576c1ebf80ff974545fd5e88d1c6df8
Paul Pavlov <pavlov1space@gmail.com>
2023-12-25 15:39:54 +0100

    implemented popup playVideo

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import Slider from './module/slider'
+import PlayVideo from './module/playVideo'
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next')
+    const playVideo = new PlayVideo('.showup .play', '.overlay')
 
     slider.render()
+    playVideo.init()
 })
